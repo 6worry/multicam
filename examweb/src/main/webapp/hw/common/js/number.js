a6b2292d48e2210f9b6ffba82ca504120470bfd6
6worry <dudfhr217@naver.com>
2024-03-06 02:49:46 +0900

    modify if statement-add negation !

--- examweb/src/main/webapp/hw/common/js/number.js
+++ examweb/src/main/webapp/hw/common/js/number.js
@@ -2,11 +2,11 @@ function number() {
 	let num = prompt("숫자를 입력하시오");
 	let negation = (num != null && !isNaN(num))
 	
-	if(num > 0 && num % 2 != 0 && negation && /\W/.test(num)) {
+	if(num > 0 && num % 2 != 0 && negation && !/\W/.test(num)) {
 		document.write("<h1>양수이면서 홀수</h1>");
-	} else if(num > 0 && num % 2 === 0 && negation && /\W/.test(num)) {
+	} else if(num > 0 && num % 2 === 0 && negation && !/\W/.test(num)) {
 		document.write("<h1>양수이면서 짝수</h1>");
-	} else if(num < 0 && negation && /[^\W-]/.test(num)) {
+	} else if(num < 0 && negation && !/[^\w-]/.test(num)) {
 		document.write("<h1>음수</h1>");
 	} else {
 		console.log(num);
