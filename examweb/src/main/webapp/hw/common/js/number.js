f61bd353b6dab1b922103798cb61ac35d8e14069
6worry <dudfhr217@naver.com>
2024-03-06 03:45:53 +0900

    add regex, debuging num ==0, refactoring

--- examweb/src/main/webapp/hw/common/js/number.js
+++ examweb/src/main/webapp/hw/common/js/number.js
@@ -1,15 +1,17 @@
 function number() {
 	let num = prompt("숫자를 입력하시오");
-	let negation = (num != null && !isNaN(num))
+	let negation = (num != null && !isNaN(num));
 	
 	if(num > 0 && num % 2 != 0 && negation && !/\W/.test(num)) {
 		document.write("<h1>양수이면서 홀수</h1>");
 	} else if(num > 0 && num % 2 === 0 && negation && !/\W/.test(num)) {
 		document.write("<h1>양수이면서 짝수</h1>");
+	} else if(num == 0) {
+		document.write("<h1>0</h1>");
 	} else if(num < 0 && negation && !/[^\w-]/.test(num)) {
 		document.write("<h1>음수</h1>");
 	} else {
 		console.log(num);
-		alert("숫자를 입력해야합나다.");
+		alert("알맞은 숫자를 입력해야합나다.");
 	};
 };
