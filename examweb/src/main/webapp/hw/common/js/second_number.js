277a5248c073c3ad21ae09f71a76275ba819653a
6worry <dudfhr217@naver.com>
2024-03-08 23:49:40 +0900

    modify negation spelling

--- examweb/src/main/webapp/hw/common/js/second_number.js
+++ examweb/src/main/webapp/hw/common/js/second_number.js
@@ -1,22 +1,22 @@
 function number() {
 	let first_num = prompt("숫자를 입력하시오");
-	let first_nagation = (first_num == null || isNaN(first_num)|| !/[^\W-.]/.test(first_num));
+	let first_negation = (first_num == null || isNaN(first_num)|| !/[^\W-.]/.test(first_num));
 	
-	if(first_nagation) {
+	if(first_negation) {
 		console.log(first_num);
 		alert("알맞은 숫자를 입력해주세요.");
 		return;
 	};
 
 	let second_num = prompt("숫자를 하나 더 입력하시오");
-	let second_nagation = (second_num == null || isNaN(second_num)|| !/[^\W-.]/.test(second_num));
+	let second_negation = (second_num == null || isNaN(second_num)|| !/[^\W-.]/.test(second_num));
 	let compare = (first_num < 10 || second_num < 10);
 	
-	if(compare && !second_nagation) {
+	if(compare && !second_negation) {
 		document.write("둘 중 최소 하나는 10 미만입니다.");
-	} else if(!compare && !second_nagation) {
+	} else if(!compare && !second_negation) {
 		document.write("둘다 10 미만이 아닙니다.");
-	} else if(second_nagation) {
+	} else if(second_negation) {
 		console.log(first_num);
 		console.log(second_num);
 		alert("알맞은 숫자를 입력해주세요.");
