40e5bfa0b0ead3f79ed0a4e736ccb2bc2f56051b
6worry <dudfhr217@naver.com>
2024-03-10 00:04:53 +0900

    perform exam-create for statement

---
+++ examweb/src/main/webapp/hw/common/js/js_multifor_test.js
@@ -0,0 +1,9 @@
+function picture() {
+	for(let i = 1; i <= 5; i++){
+		for(let j = 1; j <= i; j++){
+			document.write("<img src='./common/images/bts.jpg' width='70px;'>");
+		};
+		
+		document.write("<br />");
+	};
+};
