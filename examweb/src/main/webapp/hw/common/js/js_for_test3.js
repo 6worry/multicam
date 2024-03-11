e6ba70b65f4c31d1af0ee842ec450db0d94a8b44
6worry <dudfhr217@naver.com>
2024-03-11 23:26:55 +0900

    reset tomcat server, modify gugudan for statement

--- examweb/src/main/webapp/hw/common/js/js_for_test3.js
+++ examweb/src/main/webapp/hw/common/js/js_for_test3.js
@@ -3,33 +3,16 @@ function number() {
 	let negation = (gugudan == null || isNaN(gugudan) || /\W/.test(gugudan));
 	
 	if (negation) {
-		document.write("다시 입력하세요.");
-	} else {
-		for(let i = 1; i < 10; i++) {
-			document.write("<table border= 1px solid>");
-			if (gugudan == 1) {
-				document.write(`<tr><td style= border: 1px solid> ${gugudan} * ${i} = ` + (gugudan * i) + "</td></tr>");
-			} else if (gugudan == 2) {
-				document.write(`<tr><td style= border: 1px solid> ${gugudan} * ${i} = ` + (gugudan * i) + "</td></tr>");
-			} else if (gugudan == 3) {
-				document.write(`<tr><td style= border: 1px solid> ${gugudan} * ${i} = ` + (gugudan * i) + "</td></tr>");
-			} else if (gugudan == 4) {
-				document.write(`<tr><td style= border: 1px solid> ${gugudan} * ${i} = ` + (gugudan * i) + "</td></tr>");
-			} else if (gugudan == 5) {
-				document.write(`<tr><td style= border: 1px solid> ${gugudan} * ${i} = ` + (gugudan * i) + "</td></tr>");
-			} else if (gugudan == 6) {
-				document.write(`<tr><td style= border: 1px solid> ${gugudan} * ${i} = ` + (gugudan * i) + "</td></tr>");
-			} else if (gugudan == 7) {
-				document.write(`<tr><td style= border: 1px solid> ${gugudan} * ${i} = ` + (gugudan * i) + "</td></tr>");
-			} else if (gugudan == 8) {
-				document.write(`<tr><td style= border: 1px solid> ${gugudan} * ${i} = ` + (gugudan * i) + "</td></tr>");
-			} else if (gugudan == 9){
-				document.write(`<tr><td style= border: 1px solid> ${gugudan} * ${i} = ` + (gugudan * i) + "</td></tr>");
-			} else {
-				alert("그만");
-				return gugudan;
-			};
-			document.write("</table>");
-		};
-	};
+		alert("다시 입력하세요.");
+	} else if (gugudan >= 1 && gugudan <= 9) {
+        document.write("<table border= 1px solid>");
+
+        for (let i = 1; i < 10; i++) {
+            document.write(`<tr><td> ${gugudan} * ${i} = ` + (gugudan * i) + "</td></tr>");
+        }
+
+        document.write("</table>");
+    } else {
+        alert("그만");
+    };
 };
